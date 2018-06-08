73a323625bb6d4a01b6500f68bfb92a27c86fd7e
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 14:00:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   result = ""
   array.each_with_index do |word, index|
     
-    result << word if (index + 1) % 2 != 0
+    result << word
     result << ", " if (index + 1) % 2 == 0
   end
   return result
