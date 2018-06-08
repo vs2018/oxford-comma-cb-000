5a17147261decac8b715d810c3b09915a3b43c03
Vishal Sharma <vsh1819@gmail.com>
2018-06-08 14:04:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   result = ""
   array.each_with_index do |word, index|
-    result << "and " if array.last == word
+    result << "and " if array.last == word && array.size != 1
     result << word
     result << ", " if (array.last != word) && array.size != 1
   end
